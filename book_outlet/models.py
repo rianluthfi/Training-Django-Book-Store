9586c373025cd5b286613169731793c8afca9fbf
rianluthfi <luthfirian@gmail.com>
2024-12-25 01:19:00 +0700

    Section 6: 78. Updating Models and Migrations

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
     # id field will automatically create by Django
+
+    # add method to return show data
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
